7be7d6ff549a9861a5e9ae3de683ca815253fcaf
yu828282 <enyu0311@gmail.com>
2024-06-13 13:14:22 +0900

    change connection

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const connection = mysql.createConnection({
   host     : 'localhost',
   user     : process.env.DB_USER,
   password : process.env.DB_PASSWORD,
-  database : 'test'
+  database : process.env.DB_DATABASE
 });
 
 connection.connect((error) => {
