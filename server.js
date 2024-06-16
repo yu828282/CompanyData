4d23327f031949072a7500624fae5dfe2f63eb40
yu828282 <enyu0311@gmail.com>
2024-06-16 15:22:50 +0900

    host change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser()); // cookie-parser 미들웨어 설정
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASSWORD,
   database : process.env.DB_DATABASE
